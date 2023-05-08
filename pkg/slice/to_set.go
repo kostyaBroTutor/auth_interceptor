3fb5474da569585925066708350bc5c9bbc6b333
kostyaBro <indianor201203@gmail.com>
2023-05-08 19:40:15 +0500

    feat: add generic function slice.ToSet

---
+++ pkg/slice/to_set.go
@@ -0,0 +1,10 @@
+package slice
+
+func ToSet[T comparable](slice []T) map[T]struct{} {
+	set := make(map[T]struct{}, len(slice))
+	for _, item := range slice {
+		set[item] = struct{}{}
+	}
+
+	return set
+}
